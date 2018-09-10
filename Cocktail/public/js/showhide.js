797d4e8ae59f8e42d7653938c88b14079970c9a3
vladimirbrankovic <43136088+vladimirbrankovic@users.noreply.github.com>
2018-09-10 13:22:07 +0200

    Add files via upload

---
+++ Cocktail/public/js/showhide.js
@@ -0,0 +1,187 @@
+// JavaScript Document
+// Beaker_(cup)
+$('#show').click(function()
+{
+	$('#show').css('display','none');
+	$('#data').show();
+	$('#hide').show();
+});
+
+$('#hide').click(function()
+{
+	$('#hide').css('display','none');
+	$('#data').hide();
+	$('#show').show();
+});
+// Beer glassware
+$('#show-2').click(function()
+{
+	$('#show-2').css('display','none');
+	$('#data-2').show();
+	$('#hide-2').show();
+});
+
+$('#hide-2').click(function()
+{
+	$('#hide-2').css('display','none');
+	$('#data-2').hide();
+	$('#show-2').show();
+});
+// Coffee cup
+$('#show-3').click(function()
+{
+	$('#show-3').css('display','none');
+	$('#data-3').show();
+	$('#hide-3').show();
+});
+
+$('#hide-3').click(function()
+{
+	$('#hide-3').css('display','none');
+	$('#data-3').hide();
+	$('#show-3').show();
+});
+// Cup
+$('#show-4').click(function()
+{
+	$('#show-4').css('display','none');
+	$('#data-4').show();
+	$('#hide-4').show();
+});
+
+$('#hide-4').click(function()
+{
+	$('#hide-4').css('display','none');
+	$('#data-4').hide();
+	$('#show-4').show();
+});
+// Jar
+$('#show-5').click(function()
+{
+	$('#show-5').css('display','none');
+	$('#data-5').show();
+	$('#hide-5').show();
+});
+
+$('#hide-5').click(function()
+{
+	$('#hide-5').css('display','none');
+	$('#data-5').hide();
+	$('#show-5').show();
+});
+// Mug
+$('#show-6').click(function()
+{
+	$('#show-6').css('display','none');
+	$('#data-6').show();
+	$('#hide-6').show();
+});
+
+$('#hide-6').click(function()
+{
+	$('#hide-6').css('display','none');
+	$('#data-6').hide();
+	$('#show-6').show();
+});
+// Paper cup
+
+$('#show-7').click(function()
+{
+	$('#show-7').css('display','none');
+	$('#data-7').show();
+	$('#hide-7').show();
+});
+
+$('#hide-7').click(function()
+{
+	$('#hide-7').css('display','none');
+	$('#data-7').hide();
+	$('#show-7').show();
+});
+// Pythagorean cup
+
+$('#show-8').click(function()
+{
+	$('#show-8').css('display','none');
+	$('#data-8').show();
+	$('#hide-8').show();
+});
+
+$('#hide-8').click(function()
+{
+	$('#hide-8').css('display','none');
+	$('#data-8').hide();
+	$('#show-8').show();
+});
+// Quaich
+
+$('#show-9').click(function()
+{
+	$('#show-9').css('display','none');
+	$('#data-9').show();
+	$('#hide-9').show();
+});
+
+$('#hide-9').click(function()
+{
+	$('#hide-9').css('display','none');
+	$('#data-9').hide();
+	$('#show-9').show();
+});
+// Sake set
+
+$('#show-10').click(function()
+{
+	$('#show-10').css('display','none');
+	$('#data-10').show();
+	$('#hide-10').show();
+});
+
+$('#hide-10').click(function()
+{
+	$('#hide-10').css('display','none');
+	$('#data-10').hide();
+	$('#show-10').show();
+});
+// Stemware
+$('#show-11').click(function()
+{
+	$('#show-11').css('display','none');
+	$('#data-11').show();
+	$('#hide-11').show();
+});
+
+$('#hide-11').click(function()
+{
+	$('#hide-11').css('display','none');
+	$('#data-11').hide();
+	$('#show-11').show();
+});
+// Teacup
+$('#show-12').click(function()
+{
+	$('#show-12').css('display','none');
+	$('#data-12').show();
+	$('#hide-12').show();
+});
+
+$('#hide-12').click(function()
+{
+	$('#hide-12').css('display','none');
+	$('#data-12').hide();
+	$('#show-12').show();
+});
+// Trembleuse
+$('#show-13').click(function()
+{
+	$('#show-13').css('display','none');
+	$('#data-13').show();
+	$('#hide-13').show();
+});
+
+$('#hide-13').click(function()
+{
+	$('#hide-13').css('display','none');
+	$('#data-13').hide();
+	$('#show-13').show();
+});
